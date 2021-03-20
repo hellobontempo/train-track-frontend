222925e3cf00de0315df213b756711dc64e953b9
claire <clairebontempo@gmail.com>
2021-03-19 18:24:12 -0700

    removes old commented out code

--- src/components/ProgramButton.js
+++ src/components/ProgramButton.js
@@ -6,7 +6,6 @@ const ProgramButton = (props) => {
     return (
         <>
         <br></br>
-            {/* <Button className="block" variant="dark" href={}><Link key={props.program.id} to={`${props.url}/${props.program.id}`}>{props.buttonText}</Link></Button> */}
             <Button  key={props.program.id} className="block" variant="dark" href={`${props.url}/${props.program.id}`}>{props.buttonText}</Button>
         <br></br>
         </>
