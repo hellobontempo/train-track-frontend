e01b9b77295148b1661d89c1045883b7ab2e5c82
claire <clairebontempo@gmail.com>
2021-03-19 17:53:10 -0700

    changes ProgramsList to a function from class function

--- src/components/ProgramsList.js
+++ src/components/ProgramsList.js
@@ -1,29 +1,25 @@
-import React from "react"
-import { Button, ButtonGroup } from "react-bootstrap"
-import { Link } from 'react-router-dom'
+
 import ProgramButton from "./ProgramButton"
 
-class ProgramsList extends React.Component {
-
-    render(){
-        return (
-            <div className="formDiv">
-                <h2>Training Programs</h2><hr></hr>
-                {this.props.programs.map(program => {
-                    return (
-                        // <ButtonGroup vertical>
-                        //     <Button variant="dark"><Link key={program.id} to={`/programs/${program.id}`} > {program.title} </Link></Button>
-                            
-                        // </ButtonGroup>
-                        <>
-                            <ProgramButton program={program} url={this.props.url} buttonText={program.title}/>
-                        </>
-                    )
-                })}
-            
-            </div>
-        )
-    }
+const ProgramsList = props => {
+ 
+    return (
+        <div className="formDiv">
+            <h2>Training Programs</h2><hr></hr>
+            {props.programs.map(program => {
+                return (
+                    // <ButtonGroup vertical>
+                    //     <Button variant="dark"><Link key={program.id} to={`/programs/${program.id}`} > {program.title} </Link></Button>
+                        
+                    // </ButtonGroup>
+                    <>
+                        <ProgramButton program={program} url={props.url} buttonText={program.title}/>
+                    </>
+                )
+            })}
+        
+        </div>
+    )
 }
 
 
