55c760e7dd18c63b7a51a0b6731bd06aa0e20a29
claire <clairebontempo@gmail.com>
2021-03-19 17:30:28 -0700

    moves form into separate component

--- src/components/DisplayDate.js
+++ src/components/DisplayDate.js
@@ -3,10 +3,7 @@ import { format} from 'date-fns'
 
 const DisplayDate = (date) => {
     
-    const dateToFormat = `${date}`
-    return (
-        format(new Date(`${date}`), 'MM/d/yyyy')
-    );
+    return format(new Date(`${date}`), 'MM/d/yyyy')
 
 }
 
