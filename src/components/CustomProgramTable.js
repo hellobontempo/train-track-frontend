5ac4a8e69fc0b25603d6f1f1e8404a84d50720b1
claire <clairebontempo@gmail.com>
2021-03-21 16:23:26 -0700

    removes routes from programs container into custom programs container

---
+++ src/components/CustomProgramTable.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+
+class CustomProgramTable extends React.Component {
+
+
+    componentDidMount(){
+        console.log(this.props)
+        
+    }
+
+    render(){
+        return (
+
+            <Fragment>
+
+
+            </Fragment>
+        )
+    }
+}
+
+export default CustomProgramTable
--- src/components/UserProgramTable.js
+++ src/components/UserProgramTable.js
@@ -12,7 +12,6 @@ class UserTable extends React.Component {
     return program
   }
 
-
   
   displayTable(){
     return (
