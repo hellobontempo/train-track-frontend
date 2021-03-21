921ee200b0d44bf22991da0d48382b2914c60c76
claire <clairebontempo@gmail.com>
2021-03-21 16:21:22 -0700

    adds custom programs container component

--- src/components/containers/CustomProgramsContainer.js
+++ src/components/containers/CustomProgramsContainer.js
@@ -2,6 +2,7 @@ import Switch from "react-bootstrap/esm/Switch"
 import { Route } from "react-router"
 import UserProgramTable from "../UserProgramTable"
 import UserProgramsList from "../UserProgramsList"
+import CustomProgramTable from "../CustomProgramTable"
 
 
 const CustomProgramsContainer = (props) => {
@@ -10,8 +11,9 @@ console.log("programs container", props)
     return(
         <div>
             <Switch>
-                <Route exact path="/user_programs/:id" component={({match}) => <UserProgramTable userProgram={props.userPrograms.find(userProgram => userProgram.id === parseInt(match.params.id))}/>}/>
+                {/* <Route exact path="/user_programs/:id" component={({match}) => <UserProgramTable userProgram={props.userPrograms.find(userProgram => userProgram.id === parseInt(match.params.id))}/>}/> */}
                 <Route exact path="/user_programs" component={({match}) =>  <UserProgramsList userPrograms={props.userPrograms} url={match.url}/>}/>
+                <Route exact path="/user_programs/:id" component={({match}) =>  <CustomProgramTable userProgram={props.userPrograms.find(userProgram => userProgram.id === parseInt(match.params.id))}/>}/>
             </Switch>
         </div>
     )
