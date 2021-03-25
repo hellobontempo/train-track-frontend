361c980a65cdcb381a4e4f7c3177f822b642c6d3
claire <clairebontempo@gmail.com>
2021-03-25 08:53:28 -0700

    correctly displays custom schedule

--- src/components/table/Week.js
+++ src/components/table/Week.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import DisplayDate from '../littleHelpers/DisplayDate'
 
 
 class Week extends React.Component{
 
-    
+    findExercises(){
+      return (
+        this.props.weeklyExercises.map(workout => {
+        let exercise = this.props.exercises.find(exercise => exercise.id === workout.exercise_id)
+          return (
+            <td>
+            <u>{DisplayDate(workout.workout_date)}</u>
+            <br></br>
+            {workout.is_race_day ? 'RACE DAY!' : exercise.name}
+            <br></br>
+            {workout.miles && !workout.is_race_day ? `${workout.miles} miles` : ``}
+            </td>
+          )
+      }))
+    }
 
       render(){
         console.log(this.props)
         return (
-          this.props.weeklyExercises.map(exercise => {
-            return (
-              <td>{exercise.workout_date}</td>
-            )
-          })
-              
+              <>
+              {this.props.exercises ? this.findExercises() : <p>Loading..</p>}  
+              </>
         )}
 }
 
