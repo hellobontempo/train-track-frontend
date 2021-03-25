34af8cfa1900951368a8f8726d46e8cb4c2f55a6
claire <clairebontempo@gmail.com>
2021-03-25 11:56:15 -0700

    deletes DisplayDate component, adds displayDate function

--- src/components/littleHelpers/DisplayDate.js
+++
@@ -1,8 +0,0 @@
-import { format} from 'date-fns'
-
-
-const DisplayDate = ({date}) => {
-   return format(new Date(), 'MM/d/yyyy')
-}
-
-export default DisplayDate
---
+++ src/components/littleHelpers/helperFunctions.js
@@ -0,0 +1,10 @@
+
+
+
+export const displayDate = (date) => {
+   const array = date.split("-")
+   const displayDate = array.slice(1).concat(array[0]).join("/")
+   return (
+       displayDate
+   )
+}
