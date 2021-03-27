1d41e75a8f5d0dbca12642c52b61cb8facb5cdac
claire <clairebontempo@gmail.com>
2021-03-27 16:07:30 -0700

    removes code for rest days in form

--- src/components/ProgramForm.js
+++ src/components/ProgramForm.js
@@ -13,15 +13,6 @@ class ProgramForm extends React.Component {
         second_rest_day: 5,
         program_id: 1,
         checkedExercises: {},
-        defaultDisabled: {
-            sunday: true,
-            monday: true,
-            tuesday: false,
-            wednesday: true,
-            thursday: true,
-            friday: false,
-            saturday: true,
-        }
     }
     // componentDidMount(){
     //     this.props.fetchExercises()
@@ -63,124 +54,12 @@ class ProgramForm extends React.Component {
         })
     }
 
-    handleDateDropdownChange = ({target}) => {
-        const name = target.name
-        const value = parseInt(target.value)
-        switch (value) {
-            case 0:
-                this.setState({
-                    [name]: value,
-                    defaultDisabled: {
-                    sunday: true,
-                    monday: true,
-                    tuesday: false,
-                    wednesday: true,
-                    thursday: true,
-                    friday: false,
-                    saturday: true,
-                    }})
-            break;
-            case 1:
-                this.setState({
-                    [name]: value,
-                    defaultDisabled: {
-                        ...this.state.defaultDisabled,
-                    sunday: true,
-                    monday: true,
-                    tuesday: true,
-                    wednesday: false,
-                    thursday: true,
-                    friday: true,
-                    saturday: false,
-                    }
-            })
-            break;
-            case 2:
-                this.setState({
-                    [name]: value,
-                    defaultDisabled: {
-                    sunday: true,
-                    monday: true,
-                    tuesday: true,
-                    wednesday: true,
-                    thursday: false,
-                    friday: true,
-                    saturday: true,
-                    }
-            })
-            break;
-            case 3:
-                this.setState({
-                    [name]: value,
-                    defaultDisabled: {
-                    sunday: true,
-                    monday: false,
-                    tuesday: true,
-                    wednesday: true,
-                    thursday: true,
-                    friday: false,
-                    saturday: true,
-                    }
-            })
-            break;
-            case 4:
-                this.setState({
-                    [name]: value,
-                    defaultDisabled: {
-                    sunday: true,
-                    monday: true,
-                    tuesday: false,
-                    wednesday: true,
-                    thursday: true,
-                    friday: true,
-                    saturday: true,
-                    }
-            })
-            break;
-            case 5:
-                this.setState({
-                    [name]: value,
-                    defaultDisabled: {
-                    sunday: false,
-                    monday: true,
-                    tuesday: true,
-                    wednesday: false,
-                    thursday: true,
-                    friday: true,
-                    saturday: true,
-                    }
-            })
-            break;
-            case 6:
-                this.setState({
-                    [name]: value,
-                    defaultDisabled: {
-                    sunday: true,
-                    monday: false,
-                    tuesday: true,
-                    wednesday: true,
-                    thursday: false,
-                    friday: true,
-                    saturday: true,
-                    }
-            })
-            break;
-            default:
-                this.setState({
-                    ...this.state
-            })
-        }
-        
-    }
-
     handleOnSubmit = event => {
         event.preventDefault()
         const newProgram = {
             user_program: {
             race_date: this.state.race_date,
             username: this.state.username,
-            // first_rest_day: this.state.first_rest_day,
-            // second_rest_day: this.state.second_rest_day,
             preferred_exercise_ids: Object.keys(this.state.checkedExercises),
             program_id: 1,}
         }
@@ -193,15 +72,6 @@ class ProgramForm extends React.Component {
             second_rest_day: 5,
             program_id: 1,
             checkedExercises: {},
-            defaultDisabled: {
-                sunday: true,
-                monday: true,
-                tuesday: false,
-                wednesday: true,
-                thursday: true,
-                friday: false,
-                saturday: true,
-            }
         })
     }
     render(){
@@ -225,36 +95,6 @@ class ProgramForm extends React.Component {
                         value={this.state.username}
                         onChange={this.handleInputChange}/>
                     </label>
-                    <br></br>
-                    <label for="rest-day-one-select">Select Rest Days: </label> 
-                        <select 
-                            className="form-control"
-                            name="first_rest_day"
-                            value={this.state.first_rest_day}
-                            onChange={this.handleDateDropdownChange}>
-                            <option value="0">Sunday</option>
-                            <option value="1">Monday</option>
-                            <option value="2">Tuesday</option>
-                            <option value="3">Wednesday</option>
-                            <option value="4">Thursday</option>
-                            <option value="5">Friday</option>
-                            <option value="6">Saturday</option>
-                        </select>
-                    <p className="text-muted">Week will start on this day of the week.</p>
-                        <br></br>
-                        <select 
-                            className="form-control"
-                            name="second_rest_day"
-                            value={this.state.second_rest_day}
-                            onChange={this.handleInputChange}>
-                            <option disabled={this.state.defaultDisabled.sunday} value="0">Sunday</option>
-                            <option disabled={this.state.defaultDisabled.monday} value="1">Monday</option>
-                            <option disabled={this.state.defaultDisabled.tuesday} value="2">Tuesday</option>
-                            <option disabled={this.state.defaultDisabled.wednesday} value="3">Wednesday</option>
-                            <option disabled={this.state.defaultDisabled.thursday} value="4">Thursday</option>
-                            <option disabled={this.state.defaultDisabled.friday} value="5">Friday</option>
-                            <option disabled={this.state.defaultDisabled.saturday} value="6">Saturday</option>
-                        </select>   
                     <br></br>
                     <label for="cross_train">Choose Your Preferred Cross Training Activities:
                       {this.props.exercises ? this.renderCheckboxes() : <p>exercises coming..</p>}
