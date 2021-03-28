7fd0577f45937b4d9119a137620a6f5012f1fd03
claire <clairebontempo@gmail.com>
2021-03-28 09:19:29 -0700

    makes exercises into buttons

--- src/components/table/Week.js
+++ src/components/table/Week.js
@@ -1,15 +1,25 @@
+import { Button } from 'react-bootstrap'
 import React from 'react'
 import { connect } from 'react-redux'
 import { displayDate } from '../littleHelpers/helperFunctions'
 
+
 class Week extends React.Component{
+
 
     returnDate(workout){
       return workout.date ? workout.date : workout.workout_date
     }
 
     returnExerciseName(workout, exercise){
-      return workout.is_race_day ? 'RACE DAY!' : exercise.name
+      return workout.is_race_day ? 'RACE DAY!' : exercise.name 
+    }
+
+    handleOnClick = event => {
+      //create modal 
+      console.log(event.target.innerHTML)
+      
+
     }
 
     findExercises(){
@@ -20,7 +30,7 @@ class Week extends React.Component{
             <td>
             <p className="date-background">{displayDate(this.returnDate(workout))}</p>
             <br></br>
-            {workout.date ? <img className="table-icon" src="/images/train-icon-transparent.png" alt="train-icon"/> : this.returnExerciseName(workout, exercise)}
+            {workout.date ? <img className="table-icon" src="/images/train-icon-transparent.png" alt="train-icon"/> : <Button variant="dark" onClick={this.handleOnClick}>{this.returnExerciseName(workout, exercise)}</Button> }
             <br></br>
             {workout.miles && !workout.is_race_day ? `${workout.miles} miles` : ``}
             </td>
