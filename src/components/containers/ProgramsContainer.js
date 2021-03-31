6f3c0c2792568dfbbdf038b977b8bb10588bc93d
claire <clairebontempo@gmail.com>
2021-03-31 11:24:47 -0700

    removes UserProgramTable and rendering of ProgramTable

--- src/components/containers/ProgramsContainer.js
+++ src/components/containers/ProgramsContainer.js
@@ -7,8 +7,9 @@ console.log("programs container", props)
     return(
         <div>
             <Switch>
-               <Route exact path="/programs" component={({match}) =>  <ProgramsList programs={props.programs} url={match.url}/>}/>
-    
+                {/* <Route exact path="/programs/:id" component={({match}) =>  <ProgramTable program={props.programs.find(program => program.id === parseInt(match.params.id))}/>}/> */}
+                <Route exact path="/programs" component={({match}) =>  <ProgramsList programs={props.programs} url={match.url}/>}/>
+               
             </Switch>
         </div>
     )
