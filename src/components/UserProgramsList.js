05367b414cc805b75fb97fed78c36a86171a0528
claire <clairebontempo@gmail.com>
2021-04-01 12:18:10 -0700

    adds unique key

--- src/components/UserProgramsList.js
+++ src/components/UserProgramsList.js
@@ -8,10 +8,9 @@ const UserProgramsList = props => {
             <h2>Custom Programs</h2>
             <hr></hr>
             {props.userPrograms.map(program => {
-                console.log(program.race_date)
             return (
                 < >
-                 <ProgramButton key={`userPrograms-${program.id}`} program={program} url={props.url} buttonText={`${program.username} is racing on -- ${displayDate(program.race_date)}`}/>
+                 <ProgramButton key={`user-programs-${program.username}`} program={program} url={props.url} buttonText={`${program.username} is racing on -- ${displayDate(program.race_date)}`}/>
                 </>
             )
             })}
