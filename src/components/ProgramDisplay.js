5a148e2e98827ce09ad0c4b94b7b957e9f591b7e
claire <clairebontempo@gmail.com>
2021-04-09 16:31:13 -0700

    moves modal to program table

--- src/components/ProgramDisplay.js
+++ src/components/ProgramDisplay.js
@@ -3,7 +3,7 @@ const ProgramDisplay = ({program}) => {
     function renderProgram(){
         return (
             <div className="formDiv">
-                <h2>Training Program: {program.title}</h2>
+                <h2>{program.title}</h2>
                 <hr></hr>
                 <h3>Total Miles: {program.race_mileage}</h3>           
                 {program.description.split("-").map( e => <p>{e}</p>)}         
