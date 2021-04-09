461ec9e05429a4fc34733131cfdad81bd3684fe9
claire <clairebontempo@gmail.com>
2021-04-09 16:35:04 -0700

    removes unused displaydate

--- src/components/RunDescriptionModal.js
+++ src/components/RunDescriptionModal.js
@@ -1,6 +1,5 @@
 import { Button, Col, Modal, Row } from 'react-bootstrap'
 import React from 'react'
-import { displayDate } from '../littleHelpers/helperFunctions'
 import ProgramDisplay from './ProgramDisplay'
 
 class RunDescriptionModal extends React.Component {
