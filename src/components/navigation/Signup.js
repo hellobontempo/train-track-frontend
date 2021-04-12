7e0250259ef298d553cdca3a4f3ba2f6f581afec
claire <clairebontempo@gmail.com>
2021-04-12 11:10:09 -0700

    creates sign up form

---
+++ src/components/navigation/Signup.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+
+class Signup extends React.Component {
+
+
+    state = {
+        name: "",
+        email: "",
+        password: "",
+
+    }
+    render(){
+        return (
+            <div className="formDiv">
+                <form className="form">
+                    <input 
+                    className="form-control"
+                    name="name" 
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.handleInputChange} />
+                    <input 
+                    className="form-control"
+                    name="email" 
+                    type="email"
+                    value={this.state.email}
+                    onChange={this.handleInputChange} />
+                    onChange={this.handleInputChange} />
+                    <input 
+                    className="form-control"
+                    name="password" 
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.handleInputChange} />
+                    <input type="submit" value="Sign Up"/>
+                </form>
+            </div>
+        )
+    }
+}
