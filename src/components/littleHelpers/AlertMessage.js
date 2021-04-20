a6bf7cd51ee12b269d4959d8165a2f6356a6e64e
claire <clairebontempo@gmail.com>
2021-04-20 09:28:51 -0700

    removes sentence case function

--- src/components/littleHelpers/AlertMessage.js
+++ src/components/littleHelpers/AlertMessage.js
@@ -1,10 +1,9 @@
 import { Alert } from "react-bootstrap";
-import { sentenceCase } from "sentence-case";
 
 export default function AlertMessage (props) {
     return (
         <Alert variant={props.variant}>
-          {sentenceCase(props.message)}
+          {props.message}
         </Alert>
     )
 }
