e85cf750cb11d37a3bc5129f1b8d33d2a99288f7
claire <clairebontempo@gmail.com>
2021-04-20 10:14:48 -0700

    adds loading spinner

--- src/components/littleHelpers/AlertMessage.js
+++ src/components/littleHelpers/AlertMessage.js
@@ -1,13 +1,14 @@
 import { Alert } from "react-bootstrap";
-// import { useState } from "react";
 import { connect } from "react-redux";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function AlertMessage (props) {
   console.log(props.loading)
   console.log("message", props.message)
     return (
         <Alert variant={props.variant}>
-          {props.loading ? "Loading" : props.message.map(m => {
+          {props.loading ? <Loader type="TailSpin" color="#FD8A61" height={50} width={50} /> : props.message.map(m => {
             return <li>{m}</li>
           })}          
         </Alert>
