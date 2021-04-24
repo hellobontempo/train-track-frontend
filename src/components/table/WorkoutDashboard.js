103679d5a2ed9606f99ab7030b7f530d0e61a86e
claire <clairebontempo@gmail.com>
2021-04-24 15:49:55 -0700

    changes dashboard to functional component

--- src/components/table/WorkoutDashboard.js
+++ src/components/table/WorkoutDashboard.js
@@ -1,89 +1,73 @@
 import { Button, Col, Modal, Row } from "react-bootstrap";
-import React from "react";
+import React, { useState } from "react";
 import { displayDate } from "../utils/helperFunctions";
 import { connect } from "react-redux";
 import { editUserProgram } from "../../actions/actions";
 
-class WorkoutDashboard extends React.Component {
-  state = {
-    show: false,
-    formSelect: this.props.exercise.name,
-    customProgramId: this.props.workout.id,
+const WorkoutDashboard = (props) => {
+  const { workout, exercise } = props;
+  const [show, setShow] = useState(false);
+  const [formSelect, setFormSelect] = useState(exercise.name);
+  const [customProgramId, setCustomProgramId] = useState(workout.id);
+
+  const toggleModal = () => {
+    setShow(!show);
   };
 
-  showModal = () => {
-    this.setState({ show: true });
+  const handleChange = (e) => {
+    setFormSelect(e.target.value);
   };
 
-  hideModal = () => {
-    this.setState({ show: false });
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      formSelect: e.target.value,
-    });
-  };
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.editUserProgram(
-      this.state.formSelect,
-      this.state.customProgramId
-    );
-    this.setState({
-      formSelect: "",
-    });
+    props.editUserProgram(formSelect, customProgramId);
+    setFormSelect("");
   };
 
-  render() {
-    const { workout, exercise } = this.props;
-    return (
-      <>
-        <Button variant="dark" onClick={this.showModal}>
-          {exercise.name}
-        </Button>
-        <Modal show={this.state.show} onHide={this.hideModal} animation={false}>
-          <Modal.Header>
-            <Modal.Title>
-              Edit <u>{exercise.name}</u> on {displayDate(workout.workout_date)}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <form onSubmit={this.handleSubmit}>
-              <Row>
-                <Col>
-                  <select
-                    onChange={this.handleChange}
-                    value={this.state.formSelect}
-                    className="form-control"
-                  >
-                    {this.props.exercises.map((exercise) => {
-                      return (
-                        <option value={exercise.id}>{exercise.name}</option>
-                      );
-                    })}
-                  </select>
-                </Col>
-                <Col>
-                  <input
-                    className="form-control"
-                    type="submit"
-                    value="Edit Exercise"
-                  />
-                </Col>
-              </Row>
-            </form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.hideModal}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Button variant="dark" onClick={toggleModal}>
+        {exercise.name}
+      </Button>
+      <Modal show={show} onHide={toggleModal} animation={false}>
+        <Modal.Header>
+          <Modal.Title>
+            Edit <u>{exercise.name}</u> on {displayDate(workout.workout_date)}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+            <Row>
+              <Col>
+                <select
+                  onChange={handleChange}
+                  value={formSelect}
+                  className="form-control"
+                >
+                  {props.exercises.map((exercise) => {
+                    return <option value={exercise.id}>{exercise.name}</option>;
+                  })}
+                </select>
+              </Col>
+              <Col>
+                <input
+                  className="form-control"
+                  type="submit"
+                  value="Edit Exercise"
+                />
+              </Col>
+            </Row>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={hideModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
 const mapStateToProps = ({ exercises }) => {
   return {
